23138753264c417a8b405113d29b91be7a02a1e0
Narendra Dahile <nsdahile27@gmail.com>
2021-12-29 13:11:41 +0530

    Added api functnality

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-import pickle #to load the picked model
-from flask_cors import CORS #for deployment phase
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from flask_cors import CORS
 
 # load the model
 model = pickle.load(open('finalized_model.sav', 'rb'))
@@ -7,16 +8,42 @@ model = pickle.load(open('finalized_model.sav', 'rb'))
 # load the vectorizer
 vectorizer = pickle.load(open('vectorizer.pickle', 'rb'))
 
-#This converts the input text string to lower case
 def preprocess(text):
     text = text.lower()
-    
     return text
 
-# make a prediction using the model created
+# make a prediction
 def pred(text):
 
     predicted=(model.predict(vectorizer.transform(text)))
     return str(predicted[0])
 
-## making the routes, recive and send data using flask functionalities
+
+from flask import Flask,request, jsonify
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/predict', methods=['POST'])
+def check():
+    data = json.loads(request.data)
+    
+    text = data.get("text",None)
+    if text is None:
+        return jsonify({"message":"text not found"})
+    else:
+
+        text=preprocess(text)
+        text=[text]
+        ans=pred(text)
+        return jsonify({"predicted ":ans})
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'Machine Learning Inference'
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
